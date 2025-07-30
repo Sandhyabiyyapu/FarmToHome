25941427b8d50a010908e0c115c97ff649a9cf77
preethi-0422 <chpreethi2005@gmail.com>
2025-07-30 10:29:28 +0530

    modified

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -1,6 +1,9 @@
 const User = require('../Models/User');
 const bcrypt = require('bcryptjs');
 const Farmer = require('../Models/Farmer');
+const Admin = require('../Models/Admin');
+const jwt = require('jsonwebtoken');
+
 exports.registerCustomer = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
@@ -45,6 +48,23 @@ exports.registerFarmer = async (req, res) => {
     res.status(201).json({ message: "Farmer registered successfully. Awaiting admin approval." });
   } catch (err) {
     console.error("Farmer Registration Error:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+exports.adminLogin=async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const admin = await Admin.findOne({ email });
+    if (!admin) return res.status(404).json({ message: "Admin not found" });
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+
+    // Token (optional if you’re using auth middleware later)
+    const token = jwt.sign({ id: admin._id, role: 'admin' }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    res.json({ message: "Login successful", token });
+  } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
 };
--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { registerCustomer,registerFarmer } = require('../Controllers/authController');
+const { registerCustomer,registerFarmer,adminLogin } = require('../Controllers/authController');
 router.post('/register-customer', registerCustomer);
 router.post('/register-farmer', registerFarmer);
+router.post('/admin-login', adminLogin);
 module.exports = router;
