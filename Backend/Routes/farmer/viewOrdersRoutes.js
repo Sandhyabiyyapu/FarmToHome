67bff697540f6deb5ee10c2eeb05f86060e3bc55
unknown <sunkishalasraddha@hgmail.com>
2025-08-02 13:05:03 +0530

    Save changes before switching branches

--- Backend/Routes/farmer/viewOrdersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../../Middleware/auth');
-const ViewOrdersController = require('../../Controllers/Farmer/ViewOrdersController');
-
-router.get('/orders', auth, ViewOrdersController.getOrders);
-router.patch('/orders/:id', auth, ViewOrdersController.updateOrderStatus);
-
-module.exports = router; 
